5d573c7526cfb26d2d2383435d7381dc3d3a9dc2
savyata37 <savyataadhikari2020@gmail.com>
2026-02-17 23:42:34 +0545

    Updated backend routes, controllers, and frontend changes

--- exam-backend/src/config/db.js
+++ exam-backend/src/config/db.js
@@ -3,9 +3,9 @@
 require("dotenv").config();
 const { Pool } = require("pg");
 
-console.log("DB_HOST:", process.env.DB_HOST);
-console.log("DB_USER:", process.env.DB_USER);
-console.log("DB_PASSWORD:", process.env.DB_PASSWORD);
+// console.log("DB_HOST:", process.env.DB_HOST);
+// console.log("DB_USER:", process.env.DB_USER);
+// console.log("DB_PASSWORD:", process.env.DB_PASSWORD);
 
 const pool = new Pool({
   host: process.env.DB_HOST,
