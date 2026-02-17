17af2631282a3057581b407d142d6aea6bb6172f
savyata37 <savyataadhikari2020@gmail.com>
2026-02-18 00:50:27 +0545

    Updated backend routes, controllers, and frontend changes

--- exam-backend/src/config/env.js
+++ exam-backend/src/config/env.js
@@ -1,16 +1,29 @@
+// // require("dotenv").config();
+
+// // module.exports = {
+// //   PORT: process.env.PORT || 5000,
+// //   JWT_SECRET: process.env.JWT_SECRET
+// // };
+
+
 // require("dotenv").config();
 
 // module.exports = {
 //   PORT: process.env.PORT || 5000,
+//   DATABASE_URL: process.env.DATABASE_URL,
 //   JWT_SECRET: process.env.JWT_SECRET
 // };
 
 
-require("dotenv").config();
+// Only require dotenv locally
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
 
 module.exports = {
   PORT: process.env.PORT || 5000,
-  DATABASE_URL: process.env.DATABASE_URL,
-  JWT_SECRET: process.env.JWT_SECRET
+  JWT_SECRET: process.env.JWT_SECRET,
+  DATABASE_URL: process.env.DATABASE_URL
 };
+
 
